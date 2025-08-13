cee2f7643dbb1f79967513c860c237b40b2c6837
Navid <51497406+Navid200@users.noreply.github.com>
2025-08-13 14:19:12 -0400

    Update copy.js

--- assets/js/copy.js
+++ assets/js/copy.js
@@ -1,7 +1,9 @@
 function copyText(inputId, msgId) {
     var copyField = document.getElementById(inputId);
 
+    // Modern way: copy without selecting
     navigator.clipboard.writeText(copyField.value).then(function() {
+        // Show "Copied!" message
         var msg = document.getElementById(msgId);
         msg.style.display = 'inline';
         setTimeout(function(){
